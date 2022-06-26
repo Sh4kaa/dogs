9eb249adb92a00e3eaf1af75e842298afd3b9ff2
Dario <darioo_dario@hotmail.com>
2022-06-26 18:37:48 -0300

    validate token

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ import { UserContext } from '../UserContext'
 
 
 const Header = () => {
-  const { data } = React.useContext(UserContext)
+  const { data, userLogout } = React.useContext(UserContext)
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
@@ -15,11 +15,11 @@ const Header = () => {
         </Link>
         {data ? (<Link className={styles.login} to="/conta">
           {data.nome}
+          <button onClick={userLogout}>Sair</button>
         </Link>) : <Link className={styles.login} to="/login">
           Login / Criar login
         </Link>
         }
-
 
       </nav>
     </header>

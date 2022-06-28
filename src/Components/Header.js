a9d228f65100c9a892aade7f09ef5516e21545cf
Dario <darioo_dario@hotmail.com>
2022-06-28 17:00:01 -0300

    styles da sessão do usuario logado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ import { UserContext } from '../UserContext'
 
 
 const Header = () => {
-  const { data, userLogout } = React.useContext(UserContext)
+  const { data } = React.useContext(UserContext)
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
@@ -15,7 +15,6 @@ const Header = () => {
         </Link>
         {data ? (<Link className={styles.login} to="/conta">
           {data.nome}
-          <button onClick={userLogout}>Sair</button>
         </Link>) : <Link className={styles.login} to="/login">
           Login / Criar login
         </Link>

0bce30a6ab239374e30e111a530450cea0cf7515
Dario <darioo_dario@hotmail.com>
2022-07-11 22:24:07 -0300

    style: feed modal 2

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -6,15 +6,21 @@ import Loading from '../Helper/Loading';
 import PhotoContent from '../Photo/PhotoContent';
 import styles from './FeedModal.module.css';
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) {
+      setModalPhoto(null)
+    }
+  }
 
   React.useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
     request(url, options)
   }, [photo, request])
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -14,11 +14,11 @@ const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
-          <h1>
+          <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
           </h1>
           <ul className={styles.attributes}>
